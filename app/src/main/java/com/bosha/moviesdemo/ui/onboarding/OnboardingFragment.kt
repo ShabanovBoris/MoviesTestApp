e91f5bbdea75615d43627117d1917dec11b0a1a2
Admin <itachi_172555@mail.ru>
2021-10-03 16:50:53 +0300

    add condition/icons

--- app/src/main/java/com/bosha/moviesdemo/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/bosha/moviesdemo/ui/onboarding/OnboardingFragment.kt
@@ -1,9 +1,11 @@
 package com.bosha.moviesdemo.ui.onboarding
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import com.bosha.moviesdemo.R
 import com.bosha.moviesdemo.databinding.FragmentOnboardBinding
@@ -12,30 +14,55 @@ import com.bosha.utils.navigation.Screens
 import com.bosha.utils.navigation.navigate
 import com.google.android.material.tabs.TabLayoutMediator
 
-class OnboardingFragment: Fragment() {
+class OnboardingFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View =
-        FragmentOnboardBinding.inflate(inflater, container, false).apply {
-            setUpViewPager(this)
-        }.root
+    ): View? {
+        if (isFirstLaunch().not()) {
+            toMainScreen()
+            return null
+        }
+        setPrefValue()
+        return FragmentOnboardBinding.inflate(inflater, container, false)
+            .apply(::setUpViewPager).root
+    }
+
 
     private fun setUpViewPager(binding: FragmentOnboardBinding) {
-
         binding.viewPager.adapter = OnboardingPagerAdapter {
-            navigate {
-                target = NavCommand(Screens.MAIN)
-                options {
-                    popUpTo(R.id.nav_graph_application)
-                }
-            }
+            toMainScreen()
         }
 
         TabLayoutMediator(binding.tabLayout, binding.viewPager, true) { tab, _ ->
             tab.setIcon(R.drawable.tab_oval)
         }.attach()
+    }
+
+    private fun isFirstLaunch() = requireActivity()
+        .getSharedPreferences(KEY_PREF, Context.MODE_PRIVATE)
+        .getBoolean(KEY_IS_FIRST_LAUNCH, true)
+
+    private fun setPrefValue() {
+        requireActivity()
+            .getSharedPreferences(KEY_PREF, Context.MODE_PRIVATE).edit {
+                putBoolean(KEY_IS_FIRST_LAUNCH, false)
+            }
+    }
+
+    private fun toMainScreen(){
+        navigate {
+            target = NavCommand(Screens.MAIN)
+            options {
+                popUpTo(R.id.nav_graph_application)
+            }
+        }
+    }
+
+    private companion object {
+        const val KEY_PREF = "key_prefs_first_launch"
+        const val KEY_IS_FIRST_LAUNCH = "key_prefs_first_launch"
     }
 }
--- app/src/main/java/com/bosha/moviesdemo/ui/onboarding/OnboardingPagerAdapter.kt
+++ app/src/main/java/com/bosha/moviesdemo/ui/onboarding/OnboardingPagerAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.bosha.moviesdemo.R
 import com.bosha.moviesdemo.databinding.OnboardPagerItemBinding
 
 class OnboardingPagerAdapter(private val onOkClick: (View) -> Unit) : RecyclerView.Adapter<WelcomeViewHolder>() {
@@ -19,7 +20,20 @@ class OnboardingPagerAdapter(private val onOkClick: (View) -> Unit) : RecyclerVi
     }
 
     override fun onBindViewHolder(holder: WelcomeViewHolder, position: Int) {
-        holder.binding.tvInfo.text = position.toString()
+        when(position){
+            0 -> {
+                holder.binding.tvInfo.setText(R.string.search_spalsh_text)
+                holder.binding.ivMain.setImageResource(R.drawable.ic_outline_movie)
+            }
+            1 -> {
+                holder.binding.tvInfo.setText(R.string.favorites_spalsh_text)
+                holder.binding.ivMain.setImageResource(R.drawable.ic_favorite)
+            }
+            2 -> {
+                holder.binding.tvInfo.setText(R.string.schedule_splash_text)
+                holder.binding.ivMain.setImageResource(R.drawable.ic_schedule_24)
+            }
+        }
         holder.binding.bOk.isVisible = position == 2
     }
 
